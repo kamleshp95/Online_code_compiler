65c59cf9f8e162609fff32b714489b99173847f9
kamleshp95 <kamleshpadwal@gmail.com>
2022-06-08 20:52:17 +0530

    Initial commit

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "ace-builds/src-min-noconflict/ext-searchbox";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-jsx";
-import { connect } from 'react-redux'; 
-import { changeMode, changeName, saveFile } from '../actions/fileActions'
-import { clearErrors } from '../actions/errorActions'
-import fileDownload from 'js-file-download'     
+import { connect } from "react-redux";
+import { changeMode, changeName, saveFile } from "../actions/fileActions";
+import { clearErrors } from "../actions/errorActions";
+import fileDownload from "js-file-download";
 
-import { Button, Form, Modal, Alert } from 'react-bootstrap';
+import { Button, Form, Modal, Alert } from "react-bootstrap";
 
-const languages = [
-	"java",
-	"python",
-	"ruby",
-	"golang",
-	"csharp",
-	"elixir",
-];
+const languages = ["java", "python", "ruby", "golang", "csharp", "elixir"];
 
 const themes = [
   "monokai",
@@ -28,177 +21,182 @@ const themes = [
   "textmate",
   "solarized_dark",
   "solarized_light",
-  "terminal"
+  "terminal",
 ];
 
-languages.forEach(lang => {
+languages.forEach((lang) => {
   require(`ace-builds/src-noconflict/mode-${lang}`);
   require(`ace-builds/src-noconflict/snippets/${lang}`);
 });
 
-themes.forEach(theme => require(`ace-builds/src-noconflict/theme-${theme}`));
+themes.forEach((theme) => require(`ace-builds/src-noconflict/theme-${theme}`));
 
 class Options extends Component {
-  
-state = {
+  state = {
     open: false,
-    msg: null
-}
+    msg: null,
+  };
 
-componentDidMount() {
+  componentDidMount() {
     // Clear Errors
-    console.log('cleared errors');
+    console.log("cleared errors");
     this.props.clearErrors();
-}
+  }
 
-componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps) {
     const { error } = this.props;
     if (error !== prevProps.error) {
-        // Check for register error
-        if (error.id === 'AUTH_ERROR') {
-            this.setState({ msg: "You are not logged in" })
-        } else {
-            this.setState({ msg: null });
-        }
+      // Check for register error
+      if (error.id === "AUTH_ERROR") {
+        this.setState({ msg: "You are not logged in" });
+      } else {
+        this.setState({ msg: null });
+      }
     }
-}
+  }
 
-
-onModeChange = (e) => {
+  onModeChange = (e) => {
     this.props.changeMode(e.target.value);
-}
+  };
 
-toggle = () => {
+  toggle = () => {
     this.setState({
-        open: !this.state.open
-    })
-}
+      open: !this.state.open,
+    });
+  };
 
-onNameChange = (e) => {
+  onNameChange = (e) => {
     this.props.changeName(e.target.value);
-}
+  };
 
-onSave = () => {
+  onSave = () => {
     this.props.saveFile(this.props.userId);
     this.toggle();
-}
+  };
 
-downloadFile = () => {
-    let fileName = '', ext = '';
-    switch(this.props.file.mode)
-    {
-        case "python": ext = "py"
-                        break;
-        case "ruby": ext = "rb"
-                        break;
-        case "golang": ext = "go"
-                        break;
-        case "csharp": ext = "cs"
-                        break;
-        case "elixir": ext = "ex"
-                        break;
-        default: ext = this.props.file.mode
-                        break;
-    }  
-    if(this.props.file.name)
-        fileName = this.props.file.name + '.' + ext;
-    else
-        fileName = 'Code.' + ext;
+  downloadFile = () => {
+    let fileName = "",
+      ext = "";
+    switch (this.props.file.mode) {
+      case "python":
+        ext = "py";
+        break;
+      case "ruby":
+        ext = "rb";
+        break;
+      case "golang":
+        ext = "go";
+        break;
+      case "csharp":
+        ext = "cs";
+        break;
+      case "elixir":
+        ext = "ex";
+        break;
+      default:
+        ext = this.props.file.mode;
+        break;
+    }
+    if (this.props.file.name) fileName = this.props.file.name + "." + ext;
+    else fileName = "Code." + ext;
     fileDownload(this.props.file.value, fileName);
-}
+  };
 
-render() {
-const { theme, fontSize } = this.props.changes;
+  render() {
+    const { theme, fontSize } = this.props.changes;
 
     return (
-    <Form>
+      <Form>
         {this.state.msg ? (
-            <Alert variant='dark'>
-                { this.state.msg + '!!!'}
-            </Alert>
+          <Alert variant="dark">{this.state.msg + "!!!"}</Alert>
         ) : null}
         <Form.Group controlId="Mode" /*style={{ maxWidth: 120 }} */>
-            <Form.Label>Mode</Form.Label>
-            <Form.Control
-                as="select"
-                name="mode"
-                onChange={this.onModeChange}
-                value={this.props.file.mode}
-                className="inp"
-            >
-                {languages.map(lang => (
-                <option>
-                    {lang}
-                </option>
-                ))}
-            </Form.Control>
+          <Form.Label>Mode</Form.Label>
+          <Form.Control
+            as="select"
+            name="mode"
+            onChange={this.onModeChange}
+            value={this.props.file.mode}
+            className="inp"
+          >
+            {languages.map((lang) => (
+              <option key={lang}>{lang}</option>
+            ))}
+          </Form.Control>
         </Form.Group>
         <Form.Group controlId="Theme" /*style={{ maxWidth: 120 }} */>
-            <Form.Label>Theme</Form.Label>
-            <Form.Control
-                as="select"
-                name="theme"
-                onChange={this.props.setTheme}
-                value={theme}
-                className="inp"
-            >
-                {themes.map(theme => (
-                <option>
-                    {theme}
-                </option>
-                ))}
-            </Form.Control>
+          <Form.Label>Theme</Form.Label>
+          <Form.Control
+            as="select"
+            name="theme"
+            onChange={this.props.setTheme}
+            value={theme}
+            className="inp"
+          >
+            {themes.map((theme) => (
+              <option key={theme}>{theme}</option>
+            ))}
+          </Form.Control>
         </Form.Group>
         <Form.Group controlId="FontSize" /*style={{ maxWidth: 120 }} */>
-            <Form.Label>Font Size</Form.Label>
-            <Form.Control
-                as="select"
-                name="fontSize"
-                onChange={this.props.setFontSize}
-                value={fontSize}
-                className="inp"
-            >
-                {[14, 16, 18, 20, 24, 28, 32, 40].map(size => (
-                <option>
-                    {size}
-                </option>
-                ))}
-            </Form.Control>
+          <Form.Label>Font Size</Form.Label>
+          <Form.Control
+            as="select"
+            name="fontSize"
+            onChange={this.props.setFontSize}
+            value={fontSize}
+            className="inp"
+          >
+            {[14, 16, 18, 20, 24, 28, 32, 40].map((size) => (
+              <option key={size}>{size}</option>
+            ))}
+          </Form.Control>
         </Form.Group>
-            <Button variant='outline-secondary' onClick={this.props.run}>
-                Run
-            </Button>{' '}
-            <Button variant='outline-success' onClick={this.toggle}>
-                Save
-            </Button><br/><br/>
-            <Button variant='outline-dark' onClick={this.downloadFile} className='down-btn'>
-                Download
-            </Button>{' '}
-            <Button variant='outline-dark' onClick={this.props.onClear} className='down-btn'>
-                Reset
-            </Button><br/>
+        <Button variant="outline-secondary" onClick={this.props.run}>
+          Run
+        </Button>{" "}
+        <Button variant="outline-success" onClick={this.toggle}>
+          Save
+        </Button>
+        <br />
+        <br />
+        <Button
+          variant="outline-dark"
+          onClick={this.downloadFile}
+          className="down-btn"
+        >
+          Download
+        </Button>{" "}
+        <Button
+          variant="outline-dark"
+          onClick={this.props.onClear}
+          className="down-btn"
+        >
+          Reset
+        </Button>
+        <br />
         <Modal show={this.state.open} onHide={this.toggle}>
-            <Modal.Header closeButton>
-                <Modal.Title>Name of the File</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
+          <Modal.Header closeButton>
+            <Modal.Title>Name of the File</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
             <Form.Group controlId="FileName">
-                <Form.Label>File Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="fileName"
-                    onChange={this.onNameChange}
-                    value={this.props.file.name}
-                />
+              <Form.Label>File Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="fileName"
+                onChange={this.onNameChange}
+                value={this.props.file.name}
+              />
             </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={this.onSave}>Submit</Button>
-                <Button onClick={this.toggle}>Cancel</Button>
-            </Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.onSave}>Submit</Button>
+            <Button onClick={this.toggle}>Cancel</Button>
+          </Modal.Footer>
         </Modal>
-    </Form>
-    )
+      </Form>
+    );
   }
 }
 
@@ -207,8 +205,13 @@ const mapStateToProps = (state, oldProps) => {
     ...oldProps,
     file: state.file,
     userId: state.auth.userId,
-    error: state.error
-  }
-}
+    error: state.error,
+  };
+};
 
-export default connect(mapStateToProps, { changeMode, changeName, saveFile, clearErrors })(Options);
+export default connect(mapStateToProps, {
+  changeMode,
+  changeName,
+  saveFile,
+  clearErrors,
+})(Options);
